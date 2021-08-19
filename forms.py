9f28dbc1ed60fba117e66cd574a6829e6a892fb1
Anyijukire Janet <79198971+Anyijukire@users.noreply.github.com>
2021-08-19 22:48:00 +0300

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.db import models
-from django.forms import fields
-from .models import Form
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model=Form
-        fields="__all__"
